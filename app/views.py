caef1917cbdd4b8ed20c77d8789a98b7e50182fd
Alijah Green <atra2396@gmail.com>
2018-04-07 19:39:46 -0400

    Boilerplate done

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse(render(request, 'app/index.html'))
+
+
+def results(request):
+    # What needs to happen here?
+    #   - Data from the given website request (Facebook or Twitter) needs to be pulled, processed
+    #   - Data needs to be organized in a meaningful way: images vs text, severity
+    pass
