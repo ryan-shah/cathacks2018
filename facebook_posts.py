8e2aa8ed73abc9f5d099aa4980391163c35bb1e2
Ryan Shah <ryan.shah@uky.edu>
2018-04-07 23:51:24 -0400

    updates to google and facebook options

--- facebook_posts.py
+++ facebook_posts.py
@@ -11,18 +11,18 @@ def some_action(post):
     post's message (post['message']) or the post's picture (post['picture']).
     In this implementation we just print the post's created time.
     """
-    print(post['picture'])
+    print(post['message'])
 
 
 # You'll need an access token here to do anything.  You can get a temporary one
 # here: https://developers.facebook.com/tools/explorer/
-access_token = 'EAACEdEose0cBAIgln1th4kozhKmbbZAG0jpQNeoHzWgVxaoxCDfSAQ1tNS5UZARabbHwEl279JsGcy5QylgLoJLVZCXkZBgRiNB3dl6SMnnZA2d707D6ZAmCQV995BNl1HSQCZBvAx8VGmmbFGeuPhRCKCUdcWLUjpZC4YnD7oxlqbNacYXpj7Yr3CE2qk5MqPf9eNOPgmBmtQVZC9bDaC3stFU8hnPMpp5MZD'
+access_token = 'EAACEdEose0cBAMZB4ZBOFZCp99YY1I6whNJMlvwZBsZCKpoOt1T29nNMznY5Sz4f72y6dkUMPHBImH553q1urUa5di8DZABnRVWqKZBhVr22ZCyZChVXddJaGtKoLap3E3MUZBKoV7J32oNe2RuFZArLWg6fPv7elghfGNB1WzyPCqYpETOCSsUzy4ZAr1JjgB2SSctAPiFyaEp7DW3ZArPfwXrEYrK8EZCAot4uAZD'
 # Look at Bill Gates's profile for this example by using his Facebook id.
 user = '110355093151719'
 
 graph = facebook.GraphAPI(access_token)
 profile = graph.get_object(user)
-posts = graph.get_connections(profile['id'], 'posts')
+posts = graph.get_connections(profile['id'], 'posts', fields="message,full_picture,created_time,permalink_url")
 
 # Wrap this block in a while loop so we can keep paginating requests until
 # finished.
--- image_processing.py
+++ image_processing.py
@@ -9,7 +9,7 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = "C:\\Users\\ryan9\\Downloads\\riot-image.jpg"
+file_name = "C:\\Users\\ryan9\\Downloads\\blm-image.jpg"
 
 with io.open(file_name, 'rb') as image_file:
         content = image_file.read()
@@ -21,7 +21,17 @@ labels = response.label_annotations
 
 response2 = client.safe_search_detection(image=image)
 result = response2.safe_search_annotation
+
+response3 = client.text_detection(image=image)
+result2 = response3.text_annotations
+
+
+print('Safe Search:')
 print(result)
+
+print('Text:')
+for text in result2:
+    print(text.description)
 
 print('Labels:')
 for label in labels:
